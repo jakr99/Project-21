3bdbebb3e04fe2b20c4f1fa7a8784b429e5dbd0b
Jacob Lee <jkln3w@umsystem.edu>
2024-02-08 17:31:07 -0600

    Update file prob_1_6n.py

--- prob_1_6n.py
+++ prob_1_6n.py
@@ -1,13 +1,12 @@
 from automata.fa.dfa import DFA
 
-prob_1_6m = DFA(
+prob_1_6n = DFA(
     states={'q0', 'q1'},
     input_symbols={'0', '1', ''},
     transitions={
-        'q0': {'1': 'q0', '0': 'q0', '': 'q0'}, 
-        'q1': {'0': 'q0', '1': 'q0', '': 'q0'},
+        'q0': {'1': 'q1', '0': 'q1', '': 'q0'}, 
+        'q1': {'0': 'q1', '1': 'q1', '': 'q0'},
         },
     initial_state='q0',
     final_states={'q1'}
 )
-
